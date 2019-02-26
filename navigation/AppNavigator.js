9c7ad5dd93d199fe932e65573b14bc538d05981e
shanben7 <shannonbenson7@gmail.com>
2019-02-26 14:31:46 -0800

    remove comments in Navigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -5,14 +5,7 @@ import MainTabNavigator from './MainTabNavigator';
 import AuthScreen from '../screens/AuthScreen';
 
 export default createAppContainer(createSwitchNavigator({
-    // goto AuthScreen page by default,
-    // left MainTabNavigator alone since there are some good examples in it
+    // Read more at https://reactnavigation.org/docs/en/auth-flow.html
     Authentication: AuthScreen,
-
-    // TODO: update auth flow to something more like below
-    // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-
-
-  Main: MainTabNavigator,
+    Main: MainTabNavigator,
 }));
