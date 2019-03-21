710a07bda17d110826cbf8cb4bfe1b66060bf734
shanben7 <shannonbenson7@gmail.com>
2019-03-20 17:47:37 -0700

    DDA-20: change background colour and spacing for SignInForm

--- components/auth/SignInForm.js
+++ components/auth/SignInForm.js
@@ -1,28 +1,35 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Input, Button} from 'react-native-elements';
+import {StyleSheet, TextInput, View} from 'react-native';
+import {Button} from 'react-native-elements';
 import PropTypes from 'prop-types'
 
 const SignInForm = (props) => {
     return (
-        <View style={styles.form}>
-            <Input
-                label="Email"
-                leftIcon={{type: 'font-awesome', name: 'envelope'}}
-                onChangeText={(value) => props.onFormChange("email", value)}
-                placeholder="my@email.com"
-            />
-            <Input
-                label="Password"
-                leftIcon={{type: 'font-awesome', name: 'lock'}}
-                onChangeText={(value) => props.onFormChange("password", value)}
-                placeholder="p@ssw0rd123"
-                secureTextEntry
-            />
-            <Button
-                title='Submit'
-                onPress={props.onSubmit}
-            />
+        <View>
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    label="Email"
+                    leftIcon={{type: 'font-awesome', name: 'envelope'}}
+                    onChangeText={(value) => props.onFormChange("email", value)}
+                    placeholder="my@email.com"
+                />
+                <TextInput
+                    style={styles.input}
+                    label="Password"
+                    leftIcon={{type: 'font-awesome', name: 'lock'}}
+                    onChangeText={(value) => props.onFormChange("password", value)}
+                    placeholder="p@ssw0rd123"
+                    secureTextEntry
+                />
+            </View>
+            <View style={styles.container}>
+                {/* TODO: replace with styled button */}
+                <Button
+                    title='Submit'
+                    onPress={props.onSubmit}
+                />
+            </View>
         </View>
     );
 };
@@ -34,5 +41,17 @@ SignInForm.propTypes = {
 };
 
 const styles = StyleSheet.create({
-    form: {width: '90%'},
+    container: {
+        justifyContent: 'space-around',
+        flexDirection: 'column',
+        height: 120
+    },
+    input: {
+        backgroundColor: '#FACA8E',
+        width: 300,
+        height: 50,
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: '#F7971D'
+    }
 });
