4b1c18d392ff02448967ef1f5427935b598881bf
Katarina <kat9234@gmail.com>
2019-04-08 21:15:49 -0700

    DDA-12: delete for dda-jobs and dda-subjobs

---
+++ dda-lambdas/dda-jobs-subjobs-delete/index.js
@@ -0,0 +1,34 @@
+// Delete specified SubJob for specified Job
+// Endpoint: /jobs/{jobId}/subjobs/{subjobId}
+
+let mysql = require('mysql');
+let config = require('./config.json');
+
+let pool = mysql.createPool({
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  database: config.database
+});
+
+exports.handler = (event, context, callback) => {
+  console.log(event);
+  context.callbackWaitsForEmptyEventLoop = false;
+  pool.getConnection(function (err, connection) {
+    let sqlquery = 'DELETE FROM SubJobs WHERE jobId="' + event['pathParameters']['jobId'] + '" and subJobId="' + event['pathParameters']['subJobId'] + '";';
+    connection.query(sqlquery, function (error, results, fields) {
+      connection.release();
+      let response = {
+        "isBase64Encoded": false,
+        "statusCode": 200,
+        "headers": {},
+        "body": JSON.stringify(results)
+      }
+      if (error) {
+        callback(error);
+      } else {
+        callback(null, response);
+      }
+    });
+  });
+};
