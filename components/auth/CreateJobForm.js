35780a0a7ec8d9bed3d5956fc92404207ef95e02
Joice Tang <32604702+joitng@users.noreply.github.com>
2019-06-23 23:32:27 -0700

    Update new job form to fit mock-ups

--- components/auth/CreateJobForm.js
+++ components/auth/CreateJobForm.js
@@ -1,46 +1,24 @@
 import React from 'react';
 import {StyleSheet, TextInput, View} from 'react-native';
 import PropTypes from 'prop-types';
-import RectangleButton from "../RectangleButton";
 
 const CreateJobForm = (props) => {
     return (
-      <View style={styles.centre}>
         <View style={styles.container}>
-          <TextInput
-              style={styles.input}
-              onChangeText={(value) => props.onFormChange("jobid", value)}
-              placeholder="Job ID"
-              placeholderTextColor="grey"
-          />
-          <TextInput
-              style={styles.input}
-              onChangeText={(value) => props.onFormChange("title", value)}
-              placeholder="Title"
-              placeholderTextColor="grey"
-          />
-          <TextInput
-              style={styles.description}
-              onChangeText={(value) => props.onFormChange("description", value)}
-              placeholder="Description"
-              placeholderTextColor="grey"
-              multiline={true}
-          />
-           <View style={styles.container}>
-                <RectangleButton
-                    title="Back"
-                    backgroundColor="#F7971D"
-                    onPress={props.onSubmit}
-                />
-                <RectangleButton
-                    title="Create"
-                    backgroundColor="#F7971D"
-                    onPress={props.onSubmit}
-                />
-            </View>
+            <TextInput
+                style={styles.input}
+                onChangeText={(value) => props.onFormChange("title", value)}
+                placeholder="Title"
+                underlineColorAndroid="transparent"
+            />
+            <TextInput
+                style={styles.input}
+                onChangeText={(value) => props.onFormChange("description", value)}
+                placeholder="Description"
+                underlineColorAndroid="transparent"
+            />
         </View>
-      </View>
-  );
+    );
 }
 
 export default CreateJobForm;
@@ -54,32 +32,15 @@ const styles = StyleSheet.create({
     container: {
         justifyContent: 'space-around',
         flexDirection: 'column',
-        height: 500,
+        height: 150,
     },
     input: {
-        backgroundColor: '#FACA8E',
         width: 300,
-        height: 50,
+        height: 40,
         fontSize: 18,
-        borderRadius: 5,
-        borderWidth: 2,
-        borderColor: '#F7971D',
         textAlign: 'left',
-        paddingLeft: 15
-    },
-    description: {
-        backgroundColor: '#FACA8E',
-        width: 300,
-        height: 150,
-        fontSize: 18,
-        borderRadius: 5,
-        borderWidth: 2,
-        borderColor: '#F7971D',
-        textAlign: 'left',
-        paddingLeft: 15
-    },
-    button: {
-        justifyContent: 'space-around',
-        flexDirection: 'column',
+        borderBottomWidth: 1,
+        borderBottomColor: 'gray',
+        paddingLeft: 5,
     }
 });
