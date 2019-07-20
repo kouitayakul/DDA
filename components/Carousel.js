67057dba9ddfc88d9ad1a0910d327b3500d99f54
shanben7 <shannonbenson7@gmail.com>
2019-07-20 13:51:46 -0700

    [dda-23] remove border on bottom buttonGroup

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { Animated, View, StyleSheet, Image, Dimensions, ScrollView, Text, Button } from 'react-native';
-import { ButtonGroup, Icon } from 'react-native-elements';
+import React, {Component} from 'react';
+import {Animated, View, StyleSheet, Image, Dimensions, ScrollView, Text, Button} from 'react-native';
+import {ButtonGroup, Icon} from 'react-native-elements';
 import PropTypes from 'prop-types';
 import moment from "moment";
 
@@ -11,76 +11,78 @@ let time = 0;
 const timeArray = [];
 
 class Carousel extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    let imageArray = []
-    let buttonArray = []
-
-    const goToNext = (index) => {
-      this.scroll.scrollTo({x: index * deviceWidth});
+    constructor(props) {
+        super(props);
     }
 
-    const startTimer = () => {
-      time = moment(start).fromNow(true);
-      console.log(time);
-      timeArray.push({element: time});
-      start = moment();
+    render() {
+        let imageArray = []
+        let buttonArray = []
 
-    }
+        const goToNext = (index) => {
+            this.scroll.scrollTo({x: index * deviceWidth});
+        }
 
-    this.props.images.forEach((image, i) => {
-      const thisImage = (
-        <Image
-        key={`image${i}`}
-        source ={{uri: image}}
-        style={{ width: deviceWidth }}
-        />
-      )
-      const button = () => (
-                            <Icon
-                            name='circle'
-                            type='font-awesome'
-                            color ='#B6BF80'
-                            onPress= {() => goToNext(i)}
-                            />
-                          );
-      imageArray.push(thisImage);
-      buttonArray.push({element: button});
+        const startTimer = () => {
+            time = moment(start).fromNow(true);
+            console.log(time);
+            timeArray.push({element: time});
+            start = moment();
+
+        }
+
+        this.props.images.forEach((image, i) => {
+                const thisImage = (
+                    <Image
+                        key={`image${i}`}
+                        source={{uri: image}}
+                        style={{width: deviceWidth}}
+                    />
+                )
+                const button = () => (
+                    <Icon
+                        name='circle'
+                        type='font-awesome'
+                        color='#B6BF80'
+                        onPress={() => goToNext(i)}
+                    />
+                );
+                imageArray.push(thisImage);
+                buttonArray.push({element: button});
+            }
+        );
+
+        return (
+            <View style={styles.container}>
+                <ScrollView
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    pagingEnabled
+                    ref={(c) => this.scroll = c}
+                    onScroll={() => startTimer()}
+                >
+                    {imageArray}
+                    {timeArray}
+                </ScrollView>
+                <ButtonGroup
+                    containerStyle={{borderTopWidth: 0, borderBottomWidth: 0}}
+                    buttons={buttonArray}
+                />
+            </View>
+        )
     }
-  )
-    return (
-      <View style={styles.container}>
-      <ScrollView
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      pagingEnabled
-      ref={(c) => this.scroll = c}
-      onScroll= {() => startTimer()}
-      >
-      {imageArray}
-      {timeArray}
-      </ScrollView>
-      <ButtonGroup
-        buttons={buttonArray}
-      />
-      </View>
-    )
-  }
 }
 
 Carousel.propTypes = {
-  images: PropTypes.array
-}
+    images: PropTypes.array
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
 
 export default Carousel;
