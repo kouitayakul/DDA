336bc9e9938bd6f14ef447f624337744f44b7d47
kouitayakul <kouitayakul@gmail.com>
2020-03-18 21:36:49 -0700

    show shift summary with time for each subJob

--- src/screens/user/JobScreen.js
+++ src/screens/user/JobScreen.js
@@ -1,167 +1,174 @@
-import React from 'react';
+import React from "react";
 import {
-    Text,
-    StyleSheet,
-    View,
-    SafeAreaView,
-    FlatList,
-    TouchableHighlight,
-    AsyncStorage,
-    Button,
-    Alert
-} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import API from '../../constants/API'
-import Footer from '../../components/Footer';
+  Text,
+  StyleSheet,
+  View,
+  SafeAreaView,
+  FlatList,
+  TouchableHighlight,
+  AsyncStorage,
+  Button,
+  Alert
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import API from "../../constants/API";
+import Footer from "../../components/Footer";
 
 export default class JobScreen extends React.Component {
-    static navigationOptions = {
-          drawerLabel: () => null
-    };
+  static navigationOptions = {
+    drawerLabel: () => null
+  };
 
-    state = {
-        jobs: [],
-        shiftStarted: false,
-        shiftEnded: null,
+  state = {
+    jobs: [],
+    shiftStarted: false,
+    shiftEnded: null
+  };
+
+  async componentDidMount() {
+    try {
+      const userCode = await AsyncStorage.getItem("userCode");
+      const apiCallJobIds = await fetch(
+        API.endpoint + `users/${userCode}/jobs`
+      );
+      const jobs = await apiCallJobIds.json();
+      this.setState({
+        jobs: jobs.filter(
+          job => job.companyId === this.props.navigation.getParam("companyId")
+        )
+      });
+      var allJobs = JSON.stringify(this.state.jobs);
+      await AsyncStorage.setItem("shift", allJobs);
+      console.log(allJobs);
+
+      for (let job of this.state.jobs) {
+        var jobTitle = job.name;
+        await AsyncStorage.setItem(jobTitle, "Not Completed");
+      }
+    } catch (err) {
+      console.log(err);
     }
-    
-    async componentDidMount() {
-        try {
-            const userCode = await AsyncStorage.getItem('userCode');
-            const apiCallJobIds = await fetch(API.endpoint + `users/${userCode}/jobs`);
-            const jobs = await apiCallJobIds.json();
-            this.setState({
-                jobs: jobs.filter(job => job.companyId === this.props.navigation.getParam('companyId'))
-            });
-            var allJobs = JSON.stringify(this.state.jobs);
-            await AsyncStorage.setItem('shift', allJobs);
-            console.log(allJobs);
-            
-            for(let job of this.state.jobs) {
-                var jobTitle = job.name;
-                await AsyncStorage.setItem(jobTitle, 'Not Completed');
-            }
-        }
-        catch (err) {
-            console.log(err);
-        }
+  }
+
+  _onPressButton(jobName, jobId) {
+    if (!this.state.shiftStarted) {
+      Alert.alert("Start Shift", "Start your shift before beginning your job");
+    } else {
+      this.props.navigation.navigate("Carousel", {
+        title: jobName,
+        jobId: jobId
+      });
     }
+  }
 
-    _onPressButton(jobName, jobId) {
-        if (!this.state.shiftStarted) {
-            Alert.alert(
-                'Start Shift',
-                'Start your shift before beginning your job',
-            );
-        } else {
-            this.props.navigation.navigate('Carousel', { title: jobName, jobId: jobId })
-        }
-    };
-
-    async _onPressShiftButton(shiftStarted) {
-        console.log(shiftStarted);
-        this.setState({shiftStarted: !shiftStarted});
-        console.log(this.state.shiftStarted);
-        if (shiftStarted == true) {
-            console.log("I have started my shift");
-            shiftEnded = true;
-            console.log(shiftEnded);
-        }
-
-        if (shiftStarted == true && shiftEnded == true) {
-            console.log("I have ended my shift");
-            var test = await AsyncStorage.getItem('shift');
-            console.log(test + "getting stuff");
-            this.props.navigation.navigate('ShiftSummary');
-        } 
+  async _onPressShiftButton(shiftStarted) {
+    console.log(shiftStarted);
+    this.setState({ shiftStarted: !shiftStarted });
+    console.log(this.state.shiftStarted);
+    if (shiftStarted == true) {
+      console.log("I have started my shift");
+      shiftEnded = true;
+      console.log(shiftEnded);
     }
 
-    renderItem(item) {
-        return (
-            <TouchableHighlight onPress={() => this._onPressButton(item.name, item.jobId)}>
-                <View style={styles.jobName}>
-                    <View>
-                        <Text style={styles.title}>{item.name}</Text>
-                    </View>
-                    <Icon name='angle-right' style={styles.icon}/>
-                </View>
-            </TouchableHighlight>
-        );
-    };
-
-    render() {
-        const {jobs, shiftStarted} = this.state;
-
-        if(!jobs.length) {
-            return null;
-        }
-          
-        return (
-            <SafeAreaView style={styles.container}>
-            <View style={styles.safeAreaView}>
-                <FlatList
-                    data={jobs}
-                    renderItem={({ item }) => this.renderItem(item) }
-                    keyExtractor={item => item.jobId.toString()}
-                />
-            </View>
-            <View style={[styles.button, {backgroundColor: shiftStarted ? '#DB4848' : '#B6BF00'}]}>
-                <Button 
-                    title={shiftStarted ? 'End Shift' : 'Start Shift'}
-                    type='solid'
-                    color='#FFFFFF'
-                    onPress={() =>  this._onPressShiftButton(shiftStarted)}
-                />
-            </View>
-            <Footer 
-            info={`${jobs.length} Jobs`}
-            />
-            </SafeAreaView>
-        );
+    if (shiftStarted == true && shiftEnded == true) {
+      console.log("I have ended my shift");
+      var test = await AsyncStorage.getItem("shift");
+      console.log(test + "getting stuff");
+      this.props.navigation.navigate("ShiftSummary");
     }
+  }
+
+  renderItem(item) {
+    return (
+      <TouchableHighlight
+        onPress={() => this._onPressButton(item.name, item.jobId)}
+      >
+        <View style={styles.jobName}>
+          <View>
+            <Text style={styles.title}>{item.name}</Text>
+          </View>
+          <Icon name="angle-right" style={styles.icon} />
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
+  render() {
+    const { jobs, shiftStarted } = this.state;
+
+    if (!jobs.length) {
+      return null;
+    }
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.safeAreaView}>
+          <FlatList
+            data={jobs}
+            renderItem={({ item }) => this.renderItem(item)}
+            keyExtractor={item => item.jobId.toString()}
+          />
+        </View>
+        <View
+          style={[
+            styles.button,
+            { backgroundColor: shiftStarted ? "#DB4848" : "#B6BF00" }
+          ]}
+        >
+          <Button
+            title={shiftStarted ? "End Shift" : "Start Shift"}
+            type="solid"
+            color="#FFFFFF"
+            onPress={() => this._onPressShiftButton(shiftStarted)}
+          />
+        </View>
+        <Footer info={`${jobs.length} Jobs`} />
+      </SafeAreaView>
+    );
+  }
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFF',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'stretch',
-    },
-    safeAreaView: {
-        backgroundColor: '#F2F2F2',
-        flex: 1
-    },
-    button: {
-        height: 50,
-        marginTop: 10,
-        marginRight: 20,
-        marginBottom: 12,
-        marginLeft: 20,
-        justifyContent: 'center',
-        borderRadius: 10,
-    },
-    jobName: {
-        backgroundColor: 'rgba(255, 255, 255, 0.92)',
-        padding: 13,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        borderBottomWidth: 0.3,
-        borderBottomColor: 'grey',
-        paddingRight: 15
-      },
-      title: {
-        fontFamily: 'System',
-        fontSize: 17,
-        lineHeight: 22,
-        letterSpacing: -0.41,
-        color: "#000000"
-      },
-      icon: {
-          fontSize: 20,
-          color: '#C7C7CC'
-      }
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "stretch"
+  },
+  safeAreaView: {
+    backgroundColor: "#F2F2F2",
+    flex: 1
+  },
+  button: {
+    height: 50,
+    marginTop: 10,
+    marginRight: 20,
+    marginBottom: 12,
+    marginLeft: 20,
+    justifyContent: "center",
+    borderRadius: 10
+  },
+  jobName: {
+    backgroundColor: "rgba(255, 255, 255, 0.92)",
+    padding: 13,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 0.3,
+    borderBottomColor: "grey",
+    paddingRight: 15
+  },
+  title: {
+    fontFamily: "System",
+    fontSize: 17,
+    lineHeight: 22,
+    letterSpacing: -0.41,
+    color: "#000000"
+  },
+  icon: {
+    fontSize: 20,
+    color: "#C7C7CC"
+  }
 });
--- src/screens/user/ShiftSummaryScreen.js
+++ src/screens/user/ShiftSummaryScreen.js
@@ -1,109 +1,96 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Text, View , Dimensions, AsyncStorage}from 'react-native';
-import  RectangleButton  from '../../components/RectangleButton';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Text, View, Dimensions, AsyncStorage } from "react-native";
+import RectangleButton from "../../components/RectangleButton";
 
-const WINDOW_HEIGHT = Dimensions.get('window').height;
-
+const WINDOW_HEIGHT = Dimensions.get("window").height;
 
 export default class ShiftSummaryScreen extends React.Component {
-    static navigationOptions = {
-        drawerLabel: () => null
-    };
-    state = {
-            allJobs: [
-            ]
-    }
+  static navigationOptions = {
+    drawerLabel: () => null
+  };
+  state = {
+    allJobs: []
+  };
 
-    async componentDidMount() {
-        console.log("Got to Shift Summary")
-        try {
-            const shift = await AsyncStorage.getItem('shift');
-            console.log(shift + "i got here!!!");
-            const temp = JSON.parse(shift);
-            let shiftJobs = [];
-            for(let singleJob of temp) {
-            console.log("Got here! LP");
-            let value = singleJob.name;    
-            let firstSubJobs = await AsyncStorage.getItem(value);
-            console.log(value);
-            let objToPush = {value: firstSubJobs};
-            console.log("Line 32 of Shift SUmmary:");
-            console.log(objToPush);
-            shiftJobs.push(objToPush);
-            }
-            console.log(shiftJobs + "I have received end of COmponent Moutn");
-            this.setState({ allJobs: shiftJobs});
-        } catch(err) {
-            this.setState({error: err});
-        }
-        
-    };
-    
-    userJobs = () => {
-        console.log("I got here :O");
-        console.log(this.state.allJobs);
-        var returnObjs = [];
-        for (let [key, value] of Object.entries(this.state.allJobs)) {
-            console.log(key);
-            console.log("Line 49:" + value);
+  async componentDidMount() {
+    try {
+      const shift = await AsyncStorage.getItem("shift");
+      const temp = JSON.parse(shift);
+      let shiftJobs = [];
+      for (let singleJob of temp) {
+        let value = singleJob.name;
+        let firstSubJobs = await AsyncStorage.getItem(value);
+        let objToPush = { value: firstSubJobs };
+        shiftJobs.push(objToPush);
+      }
+      this.setState({ allJobs: shiftJobs });
+    } catch (err) {
+      this.setState({ error: err });
+    }
+  }
+
+  userJobs = () => {
+    const { allJobs } = this.state;
+    let returnObjs = [];
+
+    for (i = 0; i < allJobs.length; i++) {
+      for (let [key, value] of Object.entries(allJobs[i])) {
+        returnObjs.push(<Text style={styles.subtitle}> {key} </Text>);
+
+        if (value == "Not Completed") {
+          returnObjs.push(<Text style={styles.subtitle}> {value} </Text>);
+        } else {
+          let subjobs = JSON.parse(value);
+          for (let subjob of subjobs) {
             returnObjs.push(
-                <Text style={styles.subtitle}> {key} </Text>
-            )
-            if (value == "Not Completed") {
-                returnObjs.push(
-                    <Text style={styles.subtitle}> {value} </Text>
-                )
-            } else {
-                let subjobs = JSON.parse(JSON.stringify(value));
-                console.log(subjobs);
-                for (let subjob of subjobs) {
-                    returnObjs.push(
-                        <Text style={{textAlign:"center"}}>{subjobs.name}: {subjob.took}</Text>
-                        )
-                }
-            }
+              <Text style={{ textAlign: "center" }}>
+                {subjob.name}: {subjob.took}
+              </Text>
+            );
+          }
         }
-        return returnObjs;
+      }
     }
+    return returnObjs;
+  };
 
-    render() {
-       let completedJobs = this.userJobs;
-            return (
-                <View style={{flex:1, flexDirection: 'column', alignItems: 'center'}}>
-                    <Text style={styles.title}> Shift Summary</Text>
-                    <View style={{flex:4}}>{this.userJobs()}</View>
-                    <View style={{flex:2}}><RectangleButton title="Send email and finish" /></View>
-                </View>
-            )
-    };
-
+  render() {
+    let completedJobs = this.userJobs;
+    return (
+      <View style={{ flex: 1, flexDirection: "column", alignItems: "center" }}>
+        <Text style={styles.title}> Shift Summary</Text>
+        <View style={{ flex: 4 }}>{this.userJobs()}</View>
+        <View style={{ flex: 2 }}>
+          <RectangleButton title="Send email and finish" />
+        </View>
+      </View>
+    );
+  }
 }
 
-
 const styles = StyleSheet.create({
-    title: {
-        fontFamily: 'System',
-        fontSize: 18,
-        lineHeight: 28,
-        letterSpacing: 0.35,
-        textAlign: "center",
-        color: "#000000",
-        fontWeight: "bold",
-        justifyContent: 'space-between',
-        flex:1,
-      },
-      subtitle: {
-        fontFamily: 'System',
-        fontSize: 14,
-        lineHeight: 28,
-        letterSpacing: 0.35,
-        fontWeight: "bold",
-        color: "#000000",
-        textAlign: 'center'
-      },
-      button: {
-          bottom: 0,
-      }
+  title: {
+    fontFamily: "System",
+    fontSize: 18,
+    lineHeight: 28,
+    letterSpacing: 0.35,
+    textAlign: "center",
+    color: "#000000",
+    fontWeight: "bold",
+    justifyContent: "space-between",
+    flex: 1
+  },
+  subtitle: {
+    fontFamily: "System",
+    fontSize: 14,
+    lineHeight: 28,
+    letterSpacing: 0.35,
+    fontWeight: "bold",
+    color: "#000000",
+    textAlign: "center"
+  },
+  button: {
+    bottom: 0
+  }
 });
-
